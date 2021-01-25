873270fb3cbf5296f02a31bbb5c39e430c3c4e3c
masato1983 <50264750+masato1983@users.noreply.github.com>
2021-01-25 21:26:09 +0900

    build(gulpfile.js): implement autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const { src, dest, series, parallel } = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 function styles() {
   return src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(dest('./dist/css'))
 }
 
