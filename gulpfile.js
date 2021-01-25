75d808498bd80fcde41c4a93cb5a2aaef58f6466
masato1983 <50264750+masato1983@users.noreply.github.com>
2021-01-25 18:36:54 +0900

    Start lecture6

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,1 @@
 const { series, parallel } = require('gulp');
-
-function printName(cb) {
-  console.log('My name is Joe');
-  cb();
-}
-
-function printAge(cb) {
-  console.log('I am 30 years old');
-  cb();
-}
-
-exports.printName = printName;
-exports.build = series(printName, printAge);
