d3d2b14bc7828aff6195c099324bba08f20604ec
masato1983 <50264750+masato1983@users.noreply.github.com>
2021-01-25 20:33:10 +0900

    build(gulpfile.js): implement gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
 const { src, dest, series, parallel } = require('gulp');
+const sass = require('gulp-sass');
+
+function styles() {
+  return src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./dist/css'))
+}
+
+exports.styles = styles;
