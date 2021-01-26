556d92ccba489b4fbc3a390430b970f1042d893f
masato1983 <50264750+masato1983@users.noreply.github.com>
2021-01-26 11:43:31 +0900

    build(gulpfile.js): implement browser-sync function

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 const { watch, src, dest, series, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
 function styles() {
   return src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(dest('./dist/css'))
+
+function serve() {
+  browserSync.init({
+    server: './dist',
+    browser: 'google chrome'
+  })
 }
 
 watch('./src/sass/**/*.scss', styles)
