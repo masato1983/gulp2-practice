d30901773c6366f3cc5001ed27ea465a0d143673
masato1983 <50264750+masato1983@users.noreply.github.com>
2021-01-27 19:34:57 +0900

    build(gulpfile.js): implement pug

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,19 @@ const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
-const shell = require('gulp-shell')
+const shell = require('gulp-shell');
+const pug = require('gulp-pug');
+const plumber = require("gulp-plumber");
+
+function pugs() {
+  return src('./src/templates/pages/*.pug')
+    .pipe(plumber())
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(dest('./dist'))
+    .pipe(browserSync.stream())
+}
 
 function styles() {
   return src('./src/sass/**/*.scss')
@@ -42,6 +54,7 @@ function minifycss() {
 
 watch('./src/sass/**/*.scss', styles)
 watch('./src/js/**/*.js', webpack)
+watch('./src/templates/**/*.pug', pugs)
 
-exports.dev = series(parallel(styles, webpack), serve);
+exports.dev = series(parallel(pugs, styles, webpack), serve);
 exports.prod = minifycss;
